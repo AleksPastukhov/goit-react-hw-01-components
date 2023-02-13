592e52c0e3d8ab0b52890713c60cae5496b28039
AleksPastukhov <passandro1990@gmail.com>
2023-02-13 16:07:42 +0200

    edits according to comments

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,23 +2,30 @@ import PropTypes from 'prop-types';
 import FriendListItem from '../FriendListItem/FriendListItem';
 import { Friends } from './FriendList.styled';
 
-function FriendList(friends) {
-  return <Friends>{<FriendListItem {...friends} />}</Friends>;
+function FriendList({ friends = [] }) {
+  return (
+    <Friends>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          id={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </Friends>
+  );
 }
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
-      friends: PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.number.isRequired,
-          avatar: PropTypes.string.isRequired,
-          name: PropTypes.string.isRequired,
-          isOnline: PropTypes.bool.isRequired,
-        })
-      ),
-    })
-  ),
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export default FriendList;
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -6,29 +6,23 @@ import {
   FriendName,
 } from './FriendListItem.styled';
 
-function FriendListItem({ friends = [] }) {
+function FriendListItem({ id, avatar, name, isOnline }) {
   return (
     <>
-      {friends.map(({ id, avatar, name, isOnline }) => (
-        <Friend key={id}>
-          <FriendStatus isOnline>{isOnline}</FriendStatus>
-          <Avatar src={avatar} alt="User avatar" width="48" />
-          <FriendName>{name}</FriendName>
-        </Friend>
-      ))}
+      <Friend key={id}>
+        <FriendStatus isOnline>{isOnline}</FriendStatus>
+        <Avatar src={avatar} alt="User avatar" width="48" />
+        <FriendName>{name}</FriendName>
+      </Friend>
     </>
   );
 }
 
 FriendListItem.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-    })
-  ),
+  id: PropTypes.number.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
 
 export default FriendListItem;
